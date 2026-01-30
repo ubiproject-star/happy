d993d3b3296e3fd303dc4e0bc4a5fa44eca5acbf
Happi Builder <happi@example.com>
2026-01-30 19:02:36 +0300

    Remove debug tools and restore clean UI

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,8 @@ import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { SoundProvider } from './contexts/SoundContext.jsx'
 import { LanguageProvider } from './contexts/LanguageContext.jsx'
-import DebugLogger from './components/DebugLogger.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <DebugLogger />
     <SoundProvider>
       <LanguageProvider>
         <AuthProvider>
